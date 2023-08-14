8e7f717b14be3b5e6fd59c933a814e60d06af25f
Le Viet Hoang <levieth710@gmail.com>
2023-08-14 16:33:52 +0700

    update ễxception

---
+++ order-service/src/main/java/com/leviethoang/orderservice/exception/GlobalHandleException.java
@@ -0,0 +1,56 @@
+package com.leviethoang.orderservice.exception;
+
+import com.leviethoang.orderservice.dto.ErrorDetails;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalHandleException extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException ex , WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                ex.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String,String> errors = new HashMap<>();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        allErrors.forEach(error -> {
+            String fieldError = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldError,message);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception ex , WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                ex.getMessage(),
+                webRequest.getDescription(false)
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
---
+++ order-service/src/main/java/com/leviethoang/orderservice/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package com.leviethoang.orderservice.exception;
+
+import com.leviethoang.orderservice.dto.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private String fieldResource;
+    private String fieldError;
+    private Long fieldValue;
+
+    public ResourceNotFoundException(String fieldResource , String fieldError , Long fieldValue){
+        super(String.format("%s not found with %s: '%s'",fieldResource,fieldError,fieldValue));
+        this.fieldResource = fieldResource;
+        this.fieldError = fieldError;
+        this.fieldValue = fieldValue;
+    }
+
+}
