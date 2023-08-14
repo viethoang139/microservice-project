c9ada38a801c8c826a7790a9974c99ff3f1e3a03
Le Viet Hoang <levieth710@gmail.com>
2023-08-14 16:30:54 +0700

    update dto

--- order-service/src/main/java/com/leviethoang/orderservice/dto/OrderDto.java
+++ order-service/src/main/java/com/leviethoang/orderservice/dto/OrderDto.java
@@ -1,5 +1,8 @@
 package com.leviethoang.orderservice.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 
 @Getter
@@ -9,7 +12,11 @@ import lombok.*;
 @ToString
 public class OrderDto {
     private Long id;
+    @Min(value = 1)
     private Long productId;
+    @NotEmpty(message = "skuCode should not be null or empty")
     private String skuCode;
+    @Min(value = 1)
+    @Max(value = 10)
     private Integer quantity;
 }
